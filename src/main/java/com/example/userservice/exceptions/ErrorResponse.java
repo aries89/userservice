62d185f6634da494bc26b7391dfe235e2113dfef
Manjiri Pai <manjiri.kamat05@gmail.com>
2025-11-06 13:25:37 +1100

    Added some exceptions

--- src/main/java/com/example/userservice/exceptions/ErrorResponse.java
+++ src/main/java/com/example/userservice/exceptions/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.example.userservice.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.http.HttpStatusCode;
 
@@ -16,5 +17,16 @@ public class ErrorResponse {
 	private HttpStatusCode status;
     private String message;
     private LocalDateTime timestamp;
+    private List<String> errors;
+    
+	public ErrorResponse(HttpStatusCode status, String message, LocalDateTime timestamp) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.timestamp = timestamp;
+	}
+    
+    
+    
 
 }
--- src/main/java/com/example/userservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/userservice/exceptions/GlobalExceptionHandler.java
@@ -1,43 +1,50 @@
 package com.example.userservice.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
-        ErrorResponse response = new ErrorResponse(
-            HttpStatus.NOT_FOUND,
-            ex.getMessage(),
-            LocalDateTime.now()
-        );
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
+	public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
+		ErrorResponse response = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), LocalDateTime.now());
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
 
-    @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidPassword(InvalidPasswordException ex) {
-        ErrorResponse response = new ErrorResponse(
-            HttpStatus.BAD_REQUEST,
-            ex.getMessage(),
-            LocalDateTime.now()
-        );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
+	@ExceptionHandler(InvalidPasswordException.class)
+	public ResponseEntity<ErrorResponse> handleInvalidPassword(InvalidPasswordException ex) {
+		ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), LocalDateTime.now());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
-        ErrorResponse response = new ErrorResponse(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            "Something went wrong",
-            LocalDateTime.now()
-        );
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+				.map(err -> err.getField() + " " + err.getDefaultMessage()).toList();
+
+		ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST, "Validation failed", LocalDateTime.now(),
+				errors);
+		return ResponseEntity.badRequest().body(error);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+		ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST, "Invalid argument :"+ex.getMessage(), LocalDateTime.now());
+		return ResponseEntity.badRequest().body(error);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
+		ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong",
+				LocalDateTime.now());
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
 }
